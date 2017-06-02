7491bb89b91324437757b10d5bdd11b41e46d3ac
gpwclark <gpwclark@gmail.com>
2017-06-02 00:25:31 -0400

    adding config for greenmail

---
+++ src/test/java/org/uant/textservice/data/GreenMailConfig.java
@@ -0,0 +1,11 @@
+package org.uant.textservice.data;
+
+public final class GreenMailConfig {
+  public static final String USER_PASSWORD = "abcdef123";
+  public static final String USER_NAME = "hascode";
+  public static final String EMAIL_USER_ADDRESS = "hascode@localhost";
+  public static final String EMAIL_TO = "someone@localhost.com";
+  public static final String EMAIL_SUBJECT = "Test E-Mail";
+  public static final String EMAIL_TEXT = "This is a test e-mail.";
+  public static final String LOCALHOST = "127.0.0.1";
+ }
